83b55e1af5ae6e9c71217018aec59facedc2a8c8
Abhishek Singh <abhishekkhhsingh@gmail.com>
2026-02-15 22:00:51 +0530

    added message handling with sendMessage controller and message routes

--- BAckend/index.js
+++ BAckend/index.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import connectdb from "./config/db.js";
 import userRoute from  "./Routes/userRoute.js"
 import { connect } from "mongoose";
-
+import messageRoute from "./Routes/messageRoute.js"
 dotenv.config({});// first it will be configured
 
 const PORT = process.env.PORT || 5000;
@@ -14,6 +14,7 @@ const app = express();
 app.use(express.json()) // body parser midlware 
 app.use("/api/v1/user" , userRoute) ;// execurted on localhost:8080/api/v1/user
 
+app.use("/api/v1/message", messageRoute)
 app.listen(PORT , ()=>{
    connectdb();
    console.log(`server is listening to your port ${PORT}` );
