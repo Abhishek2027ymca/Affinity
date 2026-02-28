b74635406efcd0be161b23d11aaef904da3b280f
Abhishek Singh <abhishekkhhsingh@gmail.com>
2026-02-28 23:53:22 +0530

    completing  signup

--- Frontend/frontend/src/components/Signup.jsx
+++ Frontend/frontend/src/components/Signup.jsx
@@ -1,15 +1,75 @@
-
-import React from 'react'
-
+import React from "react";
+import {Link} from 'react-router-dom'
 
 const Signup = () => {
   return (
-    <div className = "">
-        <div>
-            <h1 className='text-3xl font-bold  text-center text-gray-300' ></h1>
-        </div>
-    </div>
-  )
-}
+    <div className="min-w-96  mx-auto">
+      <div
+        className="
+h-full w-full bg-purple-0 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-25 border border-gray-100
+  p-6  " 
+      >
+        <h1 className="text-3xl font-bold  text-center text-gray-300">
+          {" "}
+          Signup Page{" "}
+        </h1>
+        <form action="">
+            <div>
+                <label className='label p-2'>
+                 <span className= 'text-base label-text'> Full Name</span>
+                </label>
+                <input className="w-full  input-bordered h-10"  type="text" placeholder=" Your Name"/>
+            </div>
+            {/* 3 dic copies */}
+             <div>
+                <label className='label p-2'>
+                 <span className= 'text-base label-text'> UserName</span>
+                </label>
+                <input className="w-full  input-bordered h-10"  type="text" placeholder=" UserName"/>
+            </div>
 
-export default Signup
+            {/* onemore */}
+
+             <div>
+                <label className='label p-2'>
+                 <span className= 'text-base label-text'> Password </span>
+                </label>
+                <input className="w-full  input-bordered h-10"  type="password" placeholder=" Password"/>
+            </div>
+            {/* confirmpassword  */}
+
+
+             <div>
+                <label className='label p-2'>
+                 <span className= 'text-base label-text'> Confirm Password </span>
+                </label>
+                <input className="w-full  input-bordered h-10"  type="password" placeholder=" Confirm Password"/>
+            </div>
+              
+              <div  className = 'flex tems-center my-4'>
+              <div  className = 'flex items-center'>
+                <p>Male</p>
+                <input type="checkbox" defaultChecked className="checkbox mx-2" />
+             </div>
+              <div  className = 'flex items-center'>
+                <p>Female</p>
+                <input type="checkbox" defaultChecked className="checkbox mx-2" />
+             </div>
+
+              </div>
+
+
+<Link to="/login"> 
+Already have an Account? 
+</Link>
+
+<div>
+    
+</div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
