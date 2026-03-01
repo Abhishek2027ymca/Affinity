913b25f4530e65984176fda873d07d745ad0a38a
Abhishek Singh <abhishekkhhsingh@gmail.com>
2026-03-01 12:43:13 +0530

    Refactor Signup component to improve state management and form handling , checkbox storage and onsubbmit empty

--- Frontend/frontend/src/components/Signup.jsx
+++ Frontend/frontend/src/components/Signup.jsx
@@ -1,25 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useState } from "react"; // need to istall reactdom and react-router-dom fro these 
+import { useState } from "react"; // need to istall reactdom and react-router-dom fro these
 
 const Signup = () => {
+  // taking input from user and storing in state
+  const [user, setUser] = useState({
+    fullName: "",
+    username: "",
+    password: "",
+    confirmpassword: "",
+    gender: "",
+  });
 
+  const handleCheckbox = (gender) => {
+    setUser({ ...user, gender });
+  };
 
-    // taking input from user and storing in state
- const [user, setUser] = useState({
-    fullName : "",
-    username : "" ,
-    password : "",
-    confirmpassword : "",
-    gender : "" ,
- })
-
- const onSubmitHandler = (e) => {
+  const onSubmitHandler = (e) => {
     e.preventDefault();
     console.log(user);
-
- }
-
+    // after submitting my fildes s will be emptied
+    setUser({
+      fullName: "",
+      username: "",
+      password: "",
+      confirmpassword: "",
+      gender: "",
+    });
+  };
 
   return (
     <div className="min-w-96  mx-auto">
@@ -32,14 +40,14 @@ h-full w-full bg-purple-0 rounded-md bg-clip-padding backdrop-filter backdrop-bl
           {" "}
           Signup Page{" "}
         </h1>
-        <form   onSubmit =  {onSubmitHandler} action="">
+        <form onSubmit={onSubmitHandler} action="">
           <div>
             <label className="label p-2">
               <span className="text-base label-text"> Full Name</span>
             </label>
             <input
-            value ={user.fullname}
-            onChange = {(e) => setUser({...user, fullname : e.target.value})}
+              value={user.fullname}
+              onChange={(e) => setUser({ ...user, fullname: e.target.value })}
               className="w-full  input-bordered h-10"
               type="text"
               placeholder=" Your Name"
@@ -51,8 +59,8 @@ h-full w-full bg-purple-0 rounded-md bg-clip-padding backdrop-filter backdrop-bl
               <span className="text-base label-text"> UserName</span>
             </label>
             <input
-              value ={user.username}
-              onChange = {(e) => setUser({...user, username : e.target.value})}
+              value={user.username}
+              onChange={(e) => setUser({ ...user, username: e.target.value })}
               className="w-full  input-bordered h-10"
               type="text"
               placeholder=" UserName"
@@ -66,8 +74,8 @@ h-full w-full bg-purple-0 rounded-md bg-clip-padding backdrop-filter backdrop-bl
               <span className="text-base label-text"> Password </span>
             </label>
             <input
-              value ={user.password}
-              onChange = {(e) => setUser({...user, password : e.target.value})}
+              value={user.password}
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
               className="w-full  input-bordered h-10"
               type="password"
               placeholder=" Password"
@@ -80,8 +88,10 @@ h-full w-full bg-purple-0 rounded-md bg-clip-padding backdrop-filter backdrop-bl
               <span className="text-base label-text"> Confirm Password </span>
             </label>
             <input
-               value ={user.confirmpassword}
-                 onChange = {(e) => setUser({...user, confirmpassword : e.target.value})}
+              value={user.confirmpassword}
+              onChange={(e) =>
+                setUser({ ...user, confirmpassword: e.target.value })
+              }
               className="w-full  input-bordered h-10"
               type="password"
               placeholder=" Confirm Password"
@@ -91,11 +101,23 @@ h-full w-full bg-purple-0 rounded-md bg-clip-padding backdrop-filter backdrop-bl
           <div className="flex tems-center my-4">
             <div className="flex items-center">
               <p>Male</p>
-              <input type="checkbox" defaultChecked className="checkbox mx-2" />
+              <input
+                onChange={() => handleCheckbox("male")}
+                checked={user.gender == "male"}
+                type="checkbox"
+                defaultChecked
+                className="checkbox mx-2"
+              />
             </div>
             <div className="flex items-center">
               <p>Female</p>
-              <input type="checkbox" defaultChecked className="checkbox mx-2" />
+              <input
+                onChange={() => handleCheckbox("female")}
+                checked={user.gender == "female"}
+                type="checkbox"
+                defaultChecked
+                className="checkbox mx-2"
+              />
             </div>
           </div>
 
@@ -104,7 +126,10 @@ h-full w-full bg-purple-0 rounded-md bg-clip-padding backdrop-filter backdrop-bl
           </p>
 
           <div>
-            <button  type= 'submit' className="btn btn-block btn-sm mt-2 border  border-slate-700">
+            <button
+              type="submit"
+              className="btn btn-block btn-sm mt-2 border  border-slate-700"
+            >
               {" "}
               Signup
             </button>
