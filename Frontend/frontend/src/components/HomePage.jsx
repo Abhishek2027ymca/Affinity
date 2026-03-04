4bd86b76ca0d8961dc3262132c5ded5c841f24f6
Abhishek Singh <abhishekkhhsingh@gmail.com>
2026-03-04 21:11:39 +0530

    designing of hompage , modifiaction and send message part,

--- Frontend/frontend/src/components/HomePage.jsx
+++ Frontend/frontend/src/components/HomePage.jsx
@@ -1,14 +1,15 @@
-
-
-
-import React from 'react'
-
+import React from 'react';
+import Sidebar from './Sidebar';
+import MessageContainer from './MessageContainer';
+import OtherUser from './OtherUser'
 const HomePage = () => {
   return (
-    <div>
-      homepage 
+    <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+      <Sidebar />
+      <MessageContainer />
+       <OtherUser/>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
---
+++ Frontend/frontend/src/components/Sidebar.jsx
@@ -0,0 +1,23 @@
+
+import React from 'react'
+import { BiSearchAlt2 } from "react-icons/bi" ; 
+
+const Sidebar = () => {
+  return (                
+    <div className = 'border-r border-slate-500 p-4 flex flex-col'>
+      <form action ="" className = 'flex items-center gap-2'  >
+        <input className = 'input input-bordered rounded-md ' type="text"  placeholder = 'Search...' /> 
+          
+               <button type= 'submit' className = 'btn  bg-zinc-500 text-white'> 
+                  <BiSearchAlt2 className = "w-6 h-6 outline-none" > </BiSearchAlt2>
+                </button>                          
+      </form>
+     {/* complete  serach abr */}
+
+     <div className = "dividar px-3"></div>
+        
+    </div>
+  )
+}
+
+export default Sidebar
