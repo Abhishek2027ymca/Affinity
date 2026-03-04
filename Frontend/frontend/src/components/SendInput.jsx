f09686f9b30c574a60b9c176ae1e87c8b9d65078
Abhishek Singh <abhishekkhhsingh@gmail.com>
2026-03-04 23:52:01 +0530

    compete message container adde marfin and padding

--- Frontend/frontend/src/components/SendInput.jsx
+++ Frontend/frontend/src/components/SendInput.jsx
@@ -1,18 +1,24 @@
-
-import React from 'react'
+import React from 'react';
+import { IoSend } from "react-icons/io5";
 
 const SendInput = () => {
   return (
-    <div>                  
-      <form  className='px-4 my-3'>
+    <form className='px-4 my-3'>
+      <div className='w-full relative'>
+        <input
+          type="text"
+          placeholder='Send a message...'
+          className='border text-sm rounded-lg block w-full p-3 border-zinc-500 bg-gray-600 text-white'
+        />
+        <button 
+          type='submit'
+          className='absolute flex inset-y-0 end-0 items-center pr-4'
+        >
+          <IoSend />
+        </button>
+      </div>
+    </form>
+  );
+};
 
-        <div>
-            <input type="text"  placeholder ='Send a message....' 
-            className = 'border text-sm rounded-lg block w-full bg-gray-600 text-white'/>
-        </div>
-      </form>
-    </div>
-  )
-}
-
-export default SendInput
+export default SendInput;
