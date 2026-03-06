54a7fcce0993c9a25c708aa1f2fdaad91c0fdda1
Abhishek Singh <abhishekkhhsingh@gmail.com>
2026-03-06 12:07:31 +0530

    seedign with Otherusers with actual userdata, using slice

--- Frontend/frontend/src/components/OtherUsers.jsx
+++ Frontend/frontend/src/components/OtherUsers.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import OtherUser from './OtherUser.jsx';
- import useGetOtherUsers from '../hooks/useGetOtherUsers.jsx';
+import React from "react";
+import OtherUser from "./OtherUser.jsx";
+import useGetOtherUsers from "../hooks/useGetOtherUsers.jsx";
+import { useSelector } from "react-redux";
 
 const OtherUsers = () => {
-
+  // my custom hooks
   useGetOtherUsers();
+  const { OtherUsers } = useSelector((store) => store.user);
+  // early retiurn makesure iin react
+  if (!OtherUsers) return; // just return back if Other user is null
 
   return (
-   <div className ='overflow-auto'>
-    <OtherUser/>
-    
-    <OtherUser/>
-  
-    <OtherUser/>
-    <OtherUser/>
-    <OtherUser/>
-    <OtherUser/>
-    <OtherUser/>
-    <OtherUser/>
-   </div>
+    <div className="overflow-auto">
+      {OtherUsers?.map((user) => {
+        return (
+          //pasw witha key
+          <OtherUser key={user._id} user={user} />
+        );
+      })}
+    </div>
   );
 };
 
