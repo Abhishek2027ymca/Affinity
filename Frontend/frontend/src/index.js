996de2caace2c58bcc171b9e106ae7387406c649
Abhishek Singh <abhishekkhhsingh@gmail.com>
2026-03-11 14:38:52 +0530

    tr. dep.

--- Frontend/frontend/src/index.js
+++ Frontend/frontend/src/index.js
@@ -17,6 +17,7 @@ export const BASE_URL=process.env.REACT_APP_BACKEND_URL
 
 const root = ReactDOM.createRoot(document.getElementById('root')); 
 root.render(
+  
 <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
